bc4b4ca19049f14a2eb10d3373d9e02a6f5f6437
rbsa2 <rbsa2@cin.ufpe.br>
2020-08-09 21:50:02 -0300

    add targetDate

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 
-interface CountdownProps { }
+interface CountdownProps {
+  targetDate: string
+}
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div><h1>Teste Countdown</h1></div>
+  return <div> <h1>{targetDate}</h1></div>
 }
 
 Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'Data final',
+      description: 'Data final utilizada no contador',
+      type: 'string',
+      default: null,
+    },
+
+  },
 }
 
 export default Countdown

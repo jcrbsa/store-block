f4a7c025ea4d0d5ad03b9396818cb9ed91fb1541
rbsa2 <rbsa2@cin.ufpe.br>
2020-08-09 21:31:03 -0300

    First Commit

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 interface CountdownProps { }
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
-  return <div></div>
+  return <div><h1>Teste Countdown</h1></div>
 }
 
 Countdown.schema = {
